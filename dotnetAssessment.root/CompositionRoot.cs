3e15bcb6597f1c3d888849b5c89d10940e64171f
Jaime Alonso <jaimea111@gmail.com>
2023-03-13 20:56:00 -0500

    Adding latest changes to the root

--- dotnetAssessment.root/CompositionRoot.cs
+++ dotnetAssessment.root/CompositionRoot.cs
@@ -1,13 +1,26 @@
-﻿namespace dotnetAssessment.root;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using dotnetAssessment.data;
+using dotnetAssessment.business.Services;
+using dotnetAssessment.business.Services.Impl;
+using dotnetAssessment.data.Repositories;
+using dotnetAssessment.data.Repositories.Impl;
+namespace dotnetAssessment.root;
 public class CompositionRoot
 {
     public CompositionRoot()
     {
     }
 
-    public static void injectDependencies()
+    public static void injectDependencies(IServiceCollection services)
     {
-        
+        services.AddDbContext<DatabaseContext>(opts => opts.UseInMemoryDatabase("Database"));
+        services.AddScoped<DatabaseContext>();
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped<IDeveloperRepository, DeveloperRepository>();
+        services.AddScoped<IEventRepository, EventRepository>();
+        services.AddScoped<IInvitationRepository, InvitationRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
     }
 
 }
